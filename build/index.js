c3e91aed8327c291eaaece158b52c3ee189b2fc6
CheezItMan <chris@adadevelopersacademy.org>
2019-10-27 20:13:31 -0700

    initial sorter

--- build/index.js
+++ build/index.js
@@ -1,7 +1,26 @@
 "use strict";
-console.log('Heya');
-
-
-const logSomething = () => {
-    console.log('logging');
-};
+class Sorter {
+    constructor(list) {
+        this.collection = list;
+    }
+    sort() {
+        let i = 0;
+        const { length } = this.collection;
+        const { collection } = this;
+        while (i < length) {
+            let j = 0;
+            while (j < length - i - 1) {
+                if (collection[j] > collection[j + 1]) {
+                    let temp = collection[j];
+                    collection[j] = collection[j + 1];
+                    collection[j + 1] = temp;
+                }
+                j++;
+            }
+            i++;
+        }
+    }
+}
+const sorter = new Sorter([10, 15, -5, 6, 20, 1]);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,31 @@
-console.log('Heya');
 
-const logSomething = (): void => {
-  console.log('logging');
+class Sorter {
+  collection: number[];
+
+  constructor(list: number[]) {
+    this.collection = list;
+  }
+
+  sort(): void {
+    let i = 0;
+    const { length } = this.collection;
+    const { collection } = this;
+    while (i < length ) {
+      let j = 0;
+      while (j < length - i - 1) {
+        if (collection[j] > collection[j + 1]) {
+          let temp = collection[j];
+          collection[j] = collection[j + 1];
+          collection[j+1] = temp;
+        }
+        j++
+      }
+      i++;
+    }
+  }
 }
+
+
+const sorter = new Sorter([10, 15, -5, 6, 20, 1]);
+sorter.sort();
+console.log(sorter.collection);
